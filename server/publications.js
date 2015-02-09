7d2448df93882ad0a3b283d08037d86d3cf25f90
Srinivas Mangipudi <shrinimann@gmail.com>
2015-02-09 10:59:37 +0530

    added publish functions

--- server/publications.js
+++ server/publications.js
@@ -21,15 +21,128 @@ Meteor.publish('singleUser', function(id) {
 												"services.google.picture": 1}});
 });
 
-Meteor.publish('deals', function(options) {
+//get single client by id
+Meteor.publish('singleClient', function(id) {
+	check(id, String);
+	return Clients.find(id);
+});
+
+//get single client by userid
+Meteor.publish('singleClientByUserId', function(userId) {
+	check(userId, String);
+	return Clients.find({userId: userId});
+});
+
+//get single agent by id
+Meteor.publish('singleAgent', function(id) {
+	check(id, String);
+	return Agents.find(id);
+});
+
+//get single agent by userid
+Meteor.publish('singleAgentByUserId', function(userId) {
+	check(userId, String);
+	return Agents.find({userId: userId});
+});
+
+//get single agentContact by id
+Meteor.publish('singleAgentContact', function(id) {
+	check(id, String);
+	return AgentContacts.find(id);
+});
+
+//All agentContacts for an agent
+Meteor.publish('agentContacts', function(agentId, options) {
+	check(agentId, String);
 	check(options, {
 		sort: Object,
 		limit: Number
 	});
-	return Deals.find({}, options);
+
+	return AgentContacts.find({agentId: agentId}, options);
 });
 
+//get single DistributionList by id
+Meteor.publish('singleDistributionList', function(id) {
+	check(id, String);
+	return DistributionLists.find(id);
+});
+
+//All distributionLists for an agent
+Meteor.publish('distributionLists', function(agentId, options) {
+	check(agentId, String);
+	check(options, {
+		sort: Object,
+		limit: Number
+	});
+
+	return DistributionLists.find({agentId: agentId}, options);
+});
+
+//get single lead by id
+Meteor.publish('singleLead', function(id) {
+	check(id, String);
+	return Leads.find(id);
+});
+
+//All leads for an agent
+Meteor.publish('leads', function(agentId, options) {
+	check(agentId, String);
+	check(options, {
+		sort: Object,
+		limit: Number
+	});
+
+	return Leads.find({agentId: agentId}, options);
+});
+
+//get single offer by id
+Meteor.publish('singleOffer', function(id) {
+	check(id, String);
+	return Offer.find(id);
+});
+
+//All offers for an agent
+Meteor.publish('offers', function(agentId, options) {
+	check(agentId, String);
+	check(options, {
+		sort: Object,
+		limit: Number
+	});
+
+	return Offers.find({agentId: agentId}, options);
+});
+
+//get single offerChain by id
+Meteor.publish('singleOfferChain', function(id) {
+	check(id, String);
+	return OfferChains.find(id);
+});
+
+//All offerChains for an offer
+Meteor.publish('offerChains', function(offerId, options) {
+	check(offerId, String);
+	check(options, {
+		sort: Object,
+		limit: Number
+	});
+
+	return Offers.find({offerId: offerId}, options);
+});
+
+//get single deal by id
 Meteor.publish('singleDeal', function(id) {
 	check(id, String);
 	return Deal.find(id);
 });
+
+//All deals for an agent
+Meteor.publish('deals', function(agentId, options) {
+	check(agentId, String);
+	check(options, {
+		sort: Object,
+		limit: Number
+	});
+	return Deals.find({agentId: agentId}, options);
+});
+
