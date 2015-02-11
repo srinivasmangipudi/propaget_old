c81587c1ceabe5450e35203a1dec0542203e5fee
Srinivas Mangipudi <shrinimann@gmail.com>
2015-02-11 12:10:17 +0530

    added fixture methods

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,49 +1,51 @@
-if(Deals.find().count() === 0)
+if(Meteor.users.find().count() === 0)
 {
-	//create test user
-	var testuserId = Meteor.users.insert({
-		profile: {name: "TestUser"}
+	//create test users
+	var userClientId = Meteor.users.insert({
+		username: "TestClient",
+		usertype: "Client",
+		password: "123123",
+		profile: {name: "TestUser Client"}
 	});
-	var testuser = Meteor.users.findOne(testuserId);
+	var userClient = Meteor.users.findOne(userClientId);
 
-	Deals.insert({	title: "Test Deal 1",
-					agentId: "need to create",
-					clientId: "need to create",
-					type: "sell",
-					propertyInfo: "2 Bhk, no bathroom",
-					handshake: false,
+	Clients.insert({
+		userId: userClientId,
+		info: "hey this is my info"
 	});
+	var client = Clients.findOne();
+	console.log("-- Client created --");
+	console.log(client);
 
-	Deals.insert({	title: "Test Deal 2",
-					agentId: "need to create",
-					clientId: "need to create",
-					type: "buy",
-					propertyInfo: "3 Bhk, 3 bathrooms",
-					handshake: false,
+	var userAgentId = Meteor.users.insert({
+		username: "TestAgent",
+		usertype: "Agent",
+		password: "123123",
+		profile: {name: "TestUser Agent"}
 	});
+	var userAgent = Meteor.users.findOne(userAgentId);
 
-	Deals.insert({	title: "Test Deal 3",
-					agentId: "need to create",
-					clientId: "need to create",
-					type: "sell",
-					propertyInfo: "4 Bhk, nice flat",
-					handshake: false,
+	Agents.insert({
+		userId: userAgentId,
+		experience: "This is the test agent",
+		summary: "over all good ratings"
 	});
+	var agent = Agents.findOne({userId: userAgentId});
+	console.log("-- Agent created --");
+	console.log(agent);
 
-	Deals.insert({	title: "Test Deal 4",
-					agentId: "need to create",
-					clientId: "need to create",
-					type: "sell",
-					propertyInfo: "Studio, no kitchen",
-					handshake: false,
-	});
-	
-	Deals.insert({	title: "Test Deal 5",
-					agentId: "need to create",
-					clientId: "need to create",
-					type: "buy",
-					propertyInfo: "9 Bhk, luxary villa",
-					handshake: false,
-	});
+	for(i=0; i<50; i++)
+	{
+		var z = i%2;
 
+		Leads.insert({
+			agentId: agent._id,
+			clientId: client._id,
+			title: "lead title #" + i,
+			type: z===0 ? "Buy" : "Sell",
+			category: z===0 ? "Lease" : "Sale",
+			propertyInfo: "property info #" + i,
+			handshake: z===0 ? true : false
+		});
+	}
 }
