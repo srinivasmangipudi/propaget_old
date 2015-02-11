8aa9418dd70fca547cae8caf49caa160de6e2fbf
Srinivas Mangipudi <shrinimann@gmail.com>
2015-02-11 12:12:54 +0530

    added security file based on ongoworks-security package for testing easy allow/deny rules specifications

---
+++ server/security.js
@@ -0,0 +1,8 @@
+//this is based on ongoworks-security package- https://atmospherejs.com/ongoworks/security
+//use this package for simple rules, but for custom rules write your own allow deny rules
+
+// Any client may insert, update, or remove an agent if logged in
+Agents.permit(['insert', 'update']).ifLoggedIn().apply();
+
+// Any client may insert, update, or remove an client if logged in
+Clients.permit(['insert', 'update']).ifLoggedIn().apply();
