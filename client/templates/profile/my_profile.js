571457f950e908c66e479be9ddfb0de1e7ff1d4e
Srinivas Mangipudi <shrinimann@gmail.com>
2015-02-11 14:37:52 +0530

    added agent & client profile stubs and logic to render any one

--- client/templates/profile/my_profile.js
+++ client/templates/profile/my_profile.js
@@ -7,12 +7,35 @@ Template.myProfile.rendered = function() {
     //console.log(controller.params.tagName);
 };
 
+Template.myProfile.helpers({
+	isAgentProfile:function() {
+		var usertype = Session.get("usertype");
+		console.log(usertype);
+		if(usertype === "Agent")
+			return true;
+		else 
+			return false;
+	},
+
+	isClientProfile:function() {
+		var usertype = Session.get("usertype");
+		console.log(usertype);
+		if(usertype === "Client")
+			return true;
+		else 
+			return false;
+	},
+
+});
+
 Template.myProfile.events({
 	'click .agentRadio': function(e) {
 		console.log("Show Agent registration form");
+		Session.set("usertype", "Agent");
 	},
 	'click .clientRadio': function(e) {
 		console.log("Show Client registration form");
+		Session.set("usertype", "Client");
 	},
 
 });
